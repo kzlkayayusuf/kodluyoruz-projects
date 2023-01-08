728a3fb42669a9cf9bf4fedc404619ce1d4b5de7
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-01-08 19:28:35 +0300

    added java101 38th homework

---
+++ java101/boksOyunu/Fighter.java
@@ -0,0 +1,38 @@
+package java101.boksOyunu;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    Fighter(String name, int damage, int health, int weight, double dodge) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
+    }
+
+    int hit(Fighter foe) {
+        System.out.println("------------");
+        System.out.println(this.name + " => " + foe.name + " " + this.damage + " hasar vurdu.");
+
+        if (foe.dodge()) {
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+
+        if (foe.health - this.damage < 0)
+            return 0;
+
+        return foe.health - this.damage;
+    }
+
+    public boolean dodge() {
+        double randomValue = Math.random() * 100;
+        return randomValue <= this.dodge;
+    }
+
+}
---
+++ java101/boksOyunu/Ring.java
@@ -0,0 +1,72 @@
+package java101.boksOyunu;
+
+public class Ring {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public void run() {
+
+        if (checkWeight()) {
+            while (f1.health > 0 && f2.health > 0) {
+                System.out.println("======== YENİ ROUND ===========");
+                if (attack()) {
+                    f2.health = f1.hit(f2);
+                } else {
+                    f1.health = f2.hit(f1);
+                }
+                if (isWin()) {
+                    break;
+                }
+                if (attack()) {
+                    f1.health = f2.hit(f1);
+                } else {
+                    f2.health = f1.hit(f2);
+                }
+                if (isWin()) {
+                    break;
+                }
+                printScore();
+            }
+
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
+        }
+
+    }
+
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    public boolean attack() {
+        double randomNumber = Math.random() * 100;
+        return (randomNumber >= 50);
+    }
+
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçı Kazanan : " + f2.name);
+            return true;
+        } else if (f2.health == 0) {
+            System.out.println("Maçı Kazanan : " + f1.name);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void printScore() {
+        System.out.println("------------");
+        System.out.println(f1.name + " Kalan Can \t:" + f1.health);
+        System.out.println(f2.name + " Kalan Can \t:" + f2.health);
+    }
+}
