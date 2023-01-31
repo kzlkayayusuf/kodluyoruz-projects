7130589b6d5e3c53d74f2517bfbb9bf312a32ddf
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-01-31 19:16:58 +0300

    DateTime and Math class methods

--- c#/c#_101/hw9/Program.cs
+++ c#/c#_101/hw9/Program.cs
@@ -5,7 +5,9 @@ class Program
     static void Main(string[] args)
     {
         SpecialMethods methods = new SpecialMethods();
-        methods.stringMetodlar();
+        //methods.stringMetodlar();
+        //methods.dateTimeMetodlar();
+        methods.mathMetodlar();
 
         Console.ReadLine();
     }
@@ -14,6 +16,68 @@ class Program
 
 public class SpecialMethods
 {
+    public void mathMetodlar()
+    {
+        System.Console.WriteLine(Math.Abs(-25));
+        System.Console.WriteLine(Math.Sin(90));
+        System.Console.WriteLine(Math.Cos(90));
+        System.Console.WriteLine(Math.Tan(90));
+
+        System.Console.WriteLine(Math.Ceiling(2.5));
+        System.Console.WriteLine(Math.Round(2.5));
+        System.Console.WriteLine(Math.Round(2.3));
+        System.Console.WriteLine(Math.Round(2.7));
+        System.Console.WriteLine(Math.Floor(2.5));
+
+        System.Console.WriteLine(Math.Max(1, 15));
+        System.Console.WriteLine(Math.Min(1, 15));
+        System.Console.WriteLine(Math.Pow(3, 4));
+        System.Console.WriteLine(Math.Sqrt(25));
+        System.Console.WriteLine(Math.Log(25));
+        System.Console.WriteLine(Math.Exp(25));
+        System.Console.WriteLine(Math.Log10(100));
+        System.Console.WriteLine(Math.Log2(8));
+
+    }
+    public void dateTimeMetodlar()
+    {
+        System.Console.WriteLine(DateTime.Now);
+        System.Console.WriteLine(DateTime.Now.Date);
+        System.Console.WriteLine(DateTime.Now.Day);
+        System.Console.WriteLine(DateTime.Now.Month);
+        System.Console.WriteLine(DateTime.Now.Year);
+        System.Console.WriteLine(DateTime.Now.Hour);
+        System.Console.WriteLine(DateTime.Now.Minute);
+        System.Console.WriteLine(DateTime.Now.Second);
+
+        System.Console.WriteLine(DateTime.Now.DayOfWeek);
+        System.Console.WriteLine(DateTime.Now.DayOfYear);
+        System.Console.WriteLine(DateTime.Now.ToLongDateString());
+        System.Console.WriteLine(DateTime.Now.ToShortDateString());
+
+        System.Console.WriteLine(DateTime.Now.ToLongTimeString());
+        System.Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+        System.Console.WriteLine(DateTime.Now.AddDays(5));
+        System.Console.WriteLine(DateTime.Now.AddHours(12));
+        System.Console.WriteLine(DateTime.Now.AddMonths(9));
+        System.Console.WriteLine(DateTime.Now.AddSeconds(60));
+        System.Console.WriteLine(DateTime.Now.AddMilliseconds(1L));
+
+        // DateTime format
+        System.Console.WriteLine(DateTime.Now.ToString("dd"));
+        System.Console.WriteLine(DateTime.Now.ToString("ddd"));
+        System.Console.WriteLine(DateTime.Now.ToString("dddd"));
+
+        System.Console.WriteLine(DateTime.Now.ToString("MM"));
+        System.Console.WriteLine(DateTime.Now.ToString("MMM"));
+        System.Console.WriteLine(DateTime.Now.ToString("MMMM"));
+
+        System.Console.WriteLine(DateTime.Now.ToString("yy"));
+        System.Console.WriteLine(DateTime.Now.ToString("yyy"));
+        System.Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+    }
     public void stringMetodlar()
     {
         // String Metotlar
